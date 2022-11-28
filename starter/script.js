6acb97a9feb115aa46d1e9aa698750328b412883
hassan <hassanpudineh67@gmail.com>
2022-11-28 09:47:37 +0330

    feat: define model necessary variables

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close-modal');
+const modalShowBtns = document.querySelectorAll('.show-modal');
+console.log(modalShowBtns)
