9b03e100439ba9ec3e02ae3dbf7671452a3403c0
hassan <hassanpudineh67@gmail.com>
2022-11-28 13:43:10 +0330

    fix: clean repeated functions

--- starter/script.js
+++ starter/script.js
@@ -3,4 +3,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeButton = document.querySelector('.close-modal');
 const modalShowBtns = document.querySelectorAll('.show-modal');
-console.log(modalShowBtns)
+const closeModal = function(){
+      modal.classList.add('hidden')
+      overlay.classList.add('hidden')
+}
+
+const openModal = function(){
+      modal.classList.remove('hidden')
+      overlay.classList.remove('hidden')
+}
+
+modalShowBtns.forEach(item => {
+            item.addEventListener('click', function(){
+      })
+})
+
+closeButton.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal);
